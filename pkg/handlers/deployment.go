918146d88636f401ed8d66c81c0fafa39abf84d5
Nickolay Polonskyi <nickolay.polonskyi@anodot.com>
2020-12-28 13:35:06 +0200

    Added support for resourece related  events

--- pkg/handlers/deployment.go
+++ pkg/handlers/deployment.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"fmt"
+
 	"github.com/anodot/anodot-common/pkg/common"
 	"github.com/anodot/anodot-common/pkg/events"
 	"github.com/anodot/kube-events/pkg/configuration"
@@ -49,7 +50,83 @@ func (d *DeploymentHandler) EventData(event Event) ([]events.Event, error) {
 		//image changed
 		for _, newC := range newDep.Spec.Template.Spec.Containers {
 			for _, oldC := range oldDeployment.Spec.Template.Spec.Containers {
+
 				if newC.Name == oldC.Name {
+
+					if !newC.Resources.Limits.Cpu().Equal(*oldC.Resources.Limits.Cpu()) {
+
+						before := oldC.Resources.Limits.Cpu().AsDec().String()
+						after := newC.Resources.Limits.Cpu().AsDec().String()
+
+						res := events.Event{
+							Title:       fmt.Sprintf("'%s' deployment cpu limit changed", deploymentName),
+							Description: fmt.Sprintf("%s  cpu limit changed from '%s' to '%s'", deploymentName, before, after),
+							Category:    d.Category,
+							Source:      d.Source,
+							Properties: []events.EventProperties{
+								{Key: "deployment", Value: deploymentName},
+								{Key: "namespace", Value: newDep.Namespace}},
+							StartDate: common.AnodotTimestamp{Time: event.EventTime},
+						}
+
+						allEvents = append(allEvents, res)
+					}
+
+					if !newC.Resources.Requests.Cpu().Equal(*oldC.Resources.Requests.Cpu()) {
+
+						before := oldC.Resources.Requests.Cpu().AsDec().String()
+						after := newC.Resources.Requests.Cpu().AsDec().String()
+
+						res := events.Event{
+							Title:       fmt.Sprintf("'%s' deployment  cpu requests changed", deploymentName),
+							Description: fmt.Sprintf("%s  cpu requests changed from '%s' to '%s'", deploymentName, before, after),
+							Category:    d.Category,
+							Source:      d.Source,
+							Properties: []events.EventProperties{
+								{Key: "deployment", Value: deploymentName},
+								{Key: "namespace", Value: newDep.Namespace}},
+							StartDate: common.AnodotTimestamp{Time: event.EventTime},
+						}
+
+						allEvents = append(allEvents, res)
+					}
+
+					if !newC.Resources.Limits.Memory().Equal(*oldC.Resources.Limits.Memory()) {
+						before := oldC.Resources.Limits.Memory().AsDec().String()
+						after := newC.Resources.Limits.Memory().AsDec().String()
+
+						res := events.Event{
+							Title:       fmt.Sprintf("'%s' deployment memory limits changed", deploymentName),
+							Description: fmt.Sprintf("%s  memory limits changed from '%s' to '%s'", deploymentName, before, after),
+							Category:    d.Category,
+							Source:      d.Source,
+							Properties: []events.EventProperties{
+								{Key: "deployment", Value: deploymentName},
+								{Key: "namespace", Value: newDep.Namespace}},
+							StartDate: common.AnodotTimestamp{Time: event.EventTime},
+						}
+
+						allEvents = append(allEvents, res)
+					}
+
+					if !newC.Resources.Requests.Memory().Equal(*oldC.Resources.Requests.Memory()) {
+						before := oldC.Resources.Requests.Memory().AsDec().String()
+						after := newC.Resources.Requests.Memory().AsDec().String()
+
+						res := events.Event{
+							Title:       fmt.Sprintf("'%s' deployment memory requests changed", deploymentName),
+							Description: fmt.Sprintf("%s  memory requests changed from '%s' to '%s'", deploymentName, before, after),
+							Category:    d.Category,
+							Source:      d.Source,
+							Properties: []events.EventProperties{
+								{Key: "deployment", Value: deploymentName},
+								{Key: "namespace", Value: newDep.Namespace}},
+							StartDate: common.AnodotTimestamp{Time: event.EventTime},
+						}
+
+						allEvents = append(allEvents, res)
+					}
+
 					if newC.Image != oldC.Image {
 						res := events.Event{
 							Title:       fmt.Sprintf("'%s' deployment container image changed", deploymentName),
