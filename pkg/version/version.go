130e5e2f592338f587412aad7b992ccc20f155be
Nickolay Polonskyi <nickolay.polonskyi@anodot.com>
2021-01-12 16:36:52 +0200

    Version changed

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 //to update version, just set variable with new version and run `make version-set`
-var VERSION = "0.0.7"
+var VERSION = "0.0.8"
 
 //set during build
 var REVISION = "unknown"
